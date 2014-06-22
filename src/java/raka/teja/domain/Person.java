33f8f569c8973c0fd5cb93667cb620671dd30e2b
Raka Teja <rakatejaa@gmail.com>
2014-06-22 19:48:59 +0800

    create entity class person

---
+++ src/java/raka/teja/domain/Person.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package raka.teja.domain;
+
+/**
+ *
+ * @author rakateja
+ */
+public class Person 
+{
+    private int id;
+    private String noKtp;
+    private String nama;
+    private int sex;
+    private String email;
+    private String phone;
+    private String city;
+    private String bio;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the noKtp
+     */
+    public String getNoKtp() {
+        return noKtp;
+    }
+
+    /**
+     * @param noKtp the noKtp to set
+     */
+    public void setNoKtp(String noKtp) {
+        this.noKtp = noKtp;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the sex
+     */
+    public int getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex the sex to set
+     */
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the bio
+     */
+    public String getBio() {
+        return bio;
+    }
+
+    /**
+     * @param bio the bio to set
+     */
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    
+    
+}
